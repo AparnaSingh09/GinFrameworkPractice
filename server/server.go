38c6a2474fc6cc658fb962c4d872ba2388f3c848
Aparna Singh <aparna.singh1@Aparnas-MacBook-Pro.local>
2023-12-01 19:07:12 +0530

    Added a unit test to test the /books route in server.go

--- server/server.go
+++ server/server.go
@@ -1,11 +1,20 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"GinFrameworkPractice/controller"
+	"github.com/gin-gonic/gin"
+)
+
+var bookController *controller.BookController = controller.New()
 
 func SetUpServer() *gin.Engine {
 	server := gin.Default()
 
 	server.GET("/test", func(context *gin.Context) {
+		context.String(200, "endpoint working")
+	})
+
+	server.GET("/books", func(context *gin.Context) {
 		context.String(200, "endpoint working")
 	})
 
--- server/server_test.go
+++ server/server_test.go
@@ -2,17 +2,32 @@ package server
 
 import (
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+var server = SetUpServer()
+
 func TestSetUpServer(t *testing.T) {
-	server := SetUpServer()
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/test", nil)
 	server.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "endpoint working", w.Body.String())
+}
+
+func TestFindAll(t *testing.T) {
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/books", nil)
+	server.ServeHTTP(w, req)
+
+	actual, _ := ioutil.ReadAll(w.Result().Body)
+	expected := "[{\"title\":\"The Bee Sting\",\"author\":\"Paul Murray\",\"price\":\"700\"},{\"title\":\"North Woods\",\"author\":\"Daniel Mason\",\"price\":\"900\"}]"
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, string(expected), string(actual))
+
 }
